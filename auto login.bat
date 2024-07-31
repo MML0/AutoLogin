7373420b04aaf4cc26b8ff35ff98106631907411
MML0 <h1337124lat@gmail.com>
2024-07-31 21:05:50 +0430

    PC version

---
+++ auto login.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+
+set url=http://10.10.10.10:8090/login.xml
+set referer=http://10.10.10.10:8090/httpclient.html
+set mode=191
+set username=0000000
+set password=AAAAaaaa
+set producttype=0
+
+for /f "tokens=*" %%A in ('powershell -command "[Math]::floor((Get-Date).ToUniversalTime().Subtract((Get-Date).ToUniversalTime().Date).TotalSeconds)"') do set "timestamp=%%A"
+
+curl -X POST ^
+    -H "User-Agent: %user_agent%" ^
+    -H "Referer: %referer%" ^
+    -d "mode=%mode%&username=%username%&password=%password%&a=%timestamp%&producttype=%producttype%" ^
+    %url%
+
+endlocal
