fd9d9fa302691ab05b876f18ca6f89866255e276
Mohammad Mahdi Latifi <95689935+MML0@users.noreply.github.com>
2024-10-05 10:16:25 +0330

    show the message

--- auto login.bat
+++ auto login.bat
@@ -4,8 +4,8 @@ setlocal
 set url=http://10.10.10.10:8090/login.xml
 set referer=http://10.10.10.10:8090/httpclient.html
 set mode=191
-set username=0000000
-set password=AAAAaaaa
+set username=01asdxxx
+set password=ad12xxx
 set producttype=0
 
 for /f "tokens=*" %%A in ('powershell -command "[Math]::floor((Get-Date).ToUniversalTime().Subtract((Get-Date).ToUniversalTime().Date).TotalSeconds)"') do set "timestamp=%%A"
@@ -15,5 +15,7 @@ curl -X POST ^
     -H "Referer: %referer%" ^
     -d "mode=%mode%&username=%username%&password=%password%&a=%timestamp%&producttype=%producttype%" ^
     %url%
+
+timeout /t 4 /nobreak >nul
 
 endlocal
