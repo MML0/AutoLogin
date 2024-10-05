91d1399338cffbadb75b1522a201ea18be91b80e
Mohammad Mahdi Latifi <95689935+MML0@users.noreply.github.com>
2024-10-05 10:17:25 +0330

    show the message

--- auto login.bat
+++ auto login.bat
@@ -1,11 +1,13 @@
 @echo off
 setlocal
 
+::change this 
+set username=01asdxxx
+set password=ad12xxx
+
 set url=http://10.10.10.10:8090/login.xml
 set referer=http://10.10.10.10:8090/httpclient.html
 set mode=191
-set username=01asdxxx
-set password=ad12xxx
 set producttype=0
 
 for /f "tokens=*" %%A in ('powershell -command "[Math]::floor((Get-Date).ToUniversalTime().Subtract((Get-Date).ToUniversalTime().Date).TotalSeconds)"') do set "timestamp=%%A"
